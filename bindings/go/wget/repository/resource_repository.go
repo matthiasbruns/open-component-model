0b2eb668b93f2c3ca5e60f6ff8a5972e6e1d0ed0
Matthias Bruns <git@matthiasbruns.com>
2026-07-08 14:11:40 +0200

    chore: sync wget input pr
    On-behalf-of: SAP <matthias.bruns@sap.com>
    Signed-off-by: Matthias Bruns <git@matthiasbruns.com>

--- bindings/go/wget/repository/resource_repository.go
+++ bindings/go/wget/repository/resource_repository.go
@@ -9,13 +9,10 @@ import (
 	descriptor "ocm.software/open-component-model/bindings/go/descriptor/runtime"
 	"ocm.software/open-component-model/bindings/go/repository"
 	"ocm.software/open-component-model/bindings/go/runtime"
-	"ocm.software/open-component-model/bindings/go/wget/access"
 	"ocm.software/open-component-model/bindings/go/wget/internal/download"
 	accessspec "ocm.software/open-component-model/bindings/go/wget/spec/access"
 	"ocm.software/open-component-model/bindings/go/wget/spec/access/v1"
 )
-
-var wgetAccess = access.NewWgetAccess()
 
 var _ repository.ResourceRepository = (*ResourceRepository)(nil)
 
@@ -54,7 +51,30 @@ func (r *ResourceRepository) GetResourceRepositoryScheme() *runtime.Scheme {
 
 // GetResourceCredentialConsumerIdentity resolves the credential consumer identity for the given resource.
 func (r *ResourceRepository) GetResourceCredentialConsumerIdentity(ctx context.Context, resource *descriptor.Resource) (runtime.Identity, error) {
-	return wgetAccess.GetResourceCredentialConsumerIdentity(ctx, resource)
+	if resource == nil {
+		return nil, fmt.Errorf("resource is required")
+	}
+	if resource.Access == nil {
+		return nil, fmt.Errorf("resource access is required")
+	}
+
+	wget := v1.Wget{}
+	if err := r.GetResourceRepositoryScheme().Convert(resource.Access, &wget); err != nil {
+		return nil, fmt.Errorf("error converting resource access spec: %w", err)
+	}
+
+	if wget.URL == "" {
+		return nil, fmt.Errorf("url is required")
+	}
+
+	identity, err := runtime.ParseURLToIdentity(wget.URL)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing wget URL to identity: %w", err)
+	}
+
+	identity.SetType(runtime.NewUnversionedType(accessspec.WgetConsumerType))
+
+	return identity, nil
 }
 
 // DownloadResource downloads a resource from the URL specified in the wget access spec.
